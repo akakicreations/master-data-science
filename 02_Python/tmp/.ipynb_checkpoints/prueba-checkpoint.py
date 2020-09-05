d4e1faddbc7bb705138e41ba9e4807633a599502
Víctor G <akakicreations@gmail.com>
2020-09-05 09:23:47 +0200

    Actualización master

---
+++ 02_Python/tmp/.ipynb_checkpoints/prueba-checkpoint.py
@@ -0,0 +1,4 @@
+
+def edad_(x):
+    edad_calculo = 2020- int(x)
+    return edad_calculo
---
+++ 04_DataChallengue/exercises_onmyown/.ipynb_checkpoints/quit_espac-checkpoint.py
@@ -0,0 +1,6 @@
+def quit_espac(input):
+    strip_list = []
+    for i in input:
+        strip_list.append(i.strip())
+    return strip_list
+#Creo una función para quitar espacios y devolver una lista 
